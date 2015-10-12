3c1c207cb7bbaaffc416f3b90b9a8bfcaa7392e6
Георгий Шевчук <g-stan@users.noreply.github.com>
2015-10-12 23:32:43 +0500

    Fixed errors

--- roman-time.js
+++ roman-time.js
@@ -1,38 +1,43 @@
 'use strict';
-var equalNumbers = {
-    0:'-',
-    1:'I', 
-    2:'II', 
-    3:'III', 
-    4:'IV', 
-    5:'V', 
-    6:'VI', 
-    7:'VII',
-    8:'VIII',
-    9:'IX',
-    10:'X',
-    20:'XX',
-    30:'XXX',
-    40:'XL',
-    50:'L'}
-    ;
 var hours = process.argv[2];
 var minutes = process.argv[3];
-
-var check=inputCheck(hours,minutes);
-if (!check) {
-    console.log('Incorrect values');
+var isCorrect=checkInput(hours,minutes);
+if (!isCorrect.value) {
+    console.log(isCorrect.message);
 }
 else {
+	var equalNumbers = {
+        0:'-',
+        1:'I', 
+        2:'II', 
+        3:'III', 
+        4:'IV', 
+        5:'V', 
+        6:'VI', 
+        7:'VII',
+        8:'VIII',
+        9:'IX',
+        10:'X',
+        20:'XX',
+        30:'XXX',
+        40:'XL',
+        50:'L'
+    };
     console.log(arabToRoman(hours)+':'+arabToRoman(minutes));
 }
 
-function inputCheck(hours, minutes) {   
-    if (process.argv.length != 4 || hours > 23 || hours < 0 || minutes > 59 || minutes < 0 ) {
-        return false;
+function checkInput(hours, minutes) {    
+    if (process.argv.length != 4) {
+        return {value: false, message: 'Amount of arguments isn\'t right'};
+    } 
+    if (hours.length != 2 || minutes.length != 2 || (parseInt(hours)!= hours) || (parseInt(minutes)!= minutes)) {
+        return {value: false, message: 'Incorrect format of time. Use format: XX XX, where X - number'};
+    }
+    if (hours > 23 || hours < 0 || minutes > 59 || minutes < 0 ) {
+        return {value: false, message: 'Incorrect values of time. Interval for hours: 0-23, for minutes: 0-59'};
     }
     else {
-        return true;
+        return {value: true, message: 'checkInput succeeded'};
     }
 }
 
